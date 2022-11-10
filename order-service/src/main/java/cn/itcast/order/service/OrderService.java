3550a446a821c4804e0ebf2ead4d49541015ec47
tanzheng <542354934@qq.com>
2022-11-10 16:41:37 +0800

    nacos注册，网关，feign

---
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -0,0 +1,45 @@
+package cn.itcast.order.service;
+
+
+import cn.itcast.feign.clients.UserClient;
+import cn.itcast.feign.pojo.User;
+import cn.itcast.order.mapper.OrderMapper;
+import cn.itcast.order.pojo.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderMapper orderMapper;
+//    @Autowired
+//    private RestTemplate restTemplate;
+//
+//    public Order queryOrderById(Long orderId) {
+//        // 1.查询订单
+//        Order order = orderMapper.findById(orderId);
+//
+//        String url="http://userservice/user/"+order.getUserId();
+//        User user = restTemplate.getForObject(url, User.class);
+//
+//        order.setUser(user);
+//
+//        // 4.返回
+//        return order;
+//    }
+    @Autowired
+    private UserClient userClient;
+
+    public Order queryOrderById(Long orderId) {
+        // 1.查询订单
+        Order order = orderMapper.findById(orderId);
+
+        User user = userClient.findById(order.getUserId());
+
+        order.setUser(user);
+
+        // 4.返回
+        return order;
+    }
+}
